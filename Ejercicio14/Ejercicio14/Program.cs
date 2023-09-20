5ac760715fcb468906224895f32ffd7e4a773fc1
JENIFER HERNANDEZ ORREGO <jeniferhernandez275138@correo.itm.edu.co>
2023-09-19 23:12:20 -0500

    14

---
+++ Ejercicio14/Ejercicio14/Program.cs
@@ -0,0 +1,33 @@
+﻿// Elabora un algoritmo que imprima los números del 1 al 100. Que calcule la suma de todos los
+// números pares por un lado, y por otro, la de todos los impares.
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sumPares = 0;
+        int sumImpares = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(i);
+
+            if (i % 2 == 0)
+            {
+                sumPares += i;
+            }
+            else
+            {
+                sumImpares += i;
+            }
+        }
+
+        Console.WriteLine("Suma de los números pares: " + sumPares);
+        Console.WriteLine("Suma de los números impares: " + sumImpares);
+    }
+}
+    }
+}
